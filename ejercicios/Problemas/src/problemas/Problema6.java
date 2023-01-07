cef04733aceffdee90f30b8b9edc8c8fe54bcc82
Daniel Ojeda <dmojeda@utpl.edu.ec>
2023-01-07 12:48:21 -0500

    Richard Ortiz y Daniel Ojeda

--- ejercicios/Problemas/src/problemas/Problema6.java
+++ ejercicios/Problemas/src/problemas/Problema6.java
@@ -10,5 +10,45 @@ package problemas;
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+        System.out.printf("La Media es: %d\n",
+                obtenerMediaAritmetica(informacion));
+        System.out.printf("La Desviacion Estandar es: %d\n",
+                obtenerDesviacionEstandar(informacion));
+    }
+
+    public static int obtenerMediaAritmetica(int informacion[]) {
+        int suma = 0;
+        int numdatos = 9;
+        int media;
+        for (int i = 0; i < informacion.length; i++) {
+            suma = suma + informacion[i];
+
+        }
+        media = suma / numdatos;
+        return media;
+
+    }
+
+    public static int obtenerDesviacionEstandar(int informacion[]) {
+        int sumatoria;
+        int varianza = 0;
+        int desviacion;
+        int med = obtenerMediaAritmetica(informacion);
+        int numdatos = 9;
+
+        for (int i = 0; i < informacion.length; i++) {
+            sumatoria = (int) Math.pow(informacion[i] - med, 2);
+            varianza = varianza + sumatoria;
+        }
+        varianza = varianza / (numdatos);
+        desviacion = (int) Math.sqrt(varianza);
+        int redondeo = (int) (Math.rint(desviacion * 100) / 100);
+        return redondeo;
+    }
+
 }
--- ejercicios/Problemas/src/problemas/Problema7.java
+++ ejercicios/Problemas/src/problemas/Problema7.java
@@ -5,18 +5,53 @@
  */
 package problemas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // 
-        int numeroDatos = 3;
-        int[] informacion = obtenerDatos(numeroDatos);
-        for (int i = 0; i < informacion.length; i++) {
-            System.out.printf("Datos ingresado: %d\n", informacion[i]);
+        String[] lista = obtenerCiudad();
+        System.out.println("El arreglo es: ");
+        for (int i = 0; i < lista.length; i++) {
+            System.out.printf("[%s]", lista[i]);
+
+        }
+        System.out.println("\nSolo debe tener valores de 4 o 5 caracteres");
+        obtenerCaracteres(lista);
+    }
+
+    public static String[] obtenerCiudad() {
+        int numero;
+        System.out.println("Ingrese el numero de valores de su lista:");
+        numero = entrada.nextInt();
+        entrada.nextLine();
+        String[] cadena = new String[numero];
+        for (int i = 0; i < cadena.length; i++) {
+            System.out.println("Ingrese ciudades del Ecuador: ");
+            String ciudad = entrada.nextLine();
+            cadena[i] = ciudad;
+        }
+        String mensajeFinal = "";
+        for (int i = 0; i < cadena.length; i++) {
+            mensajeFinal = String.format("%s%s\n",
+                    mensajeFinal, cadena[i]);
+        }
+        return cadena;
+    }
+
+    public static void obtenerCaracteres(String[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int caracteres = a[i].length();
+            if (caracteres == 4 || caracteres == 5) {
+                System.out.printf("%s\n", a[i]);
+            }
+
         }
     }
 }
